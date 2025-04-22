ac9cd51546a8f5bf0d2c6f2e093930517094c69f
ItaiYuki <K024G0055@g.neec.ac.jp>
2025-04-22 12:12:29 +0900

    00_04_Log

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
 #include <cstdint>
+#include <format>
+#include <string>
 
 // ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
@@ -16,10 +18,52 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
   return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
+void Log(const std::string &message) { OutputDebugStringA(message.c_str()); }
+
+std ::wstring ConvertString(const std::string &str) {
+  if (str.empty()) {
+    return std::wstring();
+  }
+  auto sizeNeeded =
+      MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char *>(&str[0]),
+                          static_cast<int>(str.size()), NULL, 0);
+  if (sizeNeeded == 0) {
+    return std ::wstring();
+  }
+  std ::wstring result(sizeNeeded, 0);
+  MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char *>(&str[0]),
+                      static_cast<int>(str.size()), &result[0], sizeNeeded);
+  return result;
+}
+
+std ::string ConvertString(const std ::wstring &str) {
+  if (str.empty()) {
+    return std::string();
+  }
+
+  auto sizeNeeded =
+      WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+                          NULL, 0, NULL, NULL);
+  if (sizeNeeded == 0) {
+    return std::string();
+  }
+  std ::string result(sizeNeeded, 0);
+  WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()),
+                      result.data(), sizeNeeded, NULL, NULL);
+  return result;
+}
+
 // Windowsアプリでのエントリーポイント( main関数 )
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
   // 出力ウィンドウへの文字出力
   OutputDebugStringA("Hello,DirectX!\n");
+
+  // 文字列を格納する
+  std::string str0 {"STRING!!!"};
+
+  // 整列を文字列にする
+  std::string str1{std::to_string(10)};
 
   WNDCLASS wc{};
   // ウィンドウプロシージャ
@@ -44,20 +88,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
   // クライアント領域をもとに実際のサイズにwrcを変更してもらう
   AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-
   // ウィンドウの生成
   HWND hwnd =
-      CreateWindow(wc.lpszClassName,
-                   L"CG2",
-                   WS_OVERLAPPEDWINDOW, 
-                   CW_USEDEFAULT,
-                   CW_USEDEFAULT, 
-                   wrc.right - wrc.left, 
-                   wrc.bottom - wrc.top,
-                   nullptr,
-                   nullptr,
-                   wc.hInstance,
-                   nullptr);
+      CreateWindow(wc.lpszClassName, L"CG2", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                   CW_USEDEFAULT, wrc.right - wrc.left, wrc.bottom - wrc.top,
+                   nullptr, nullptr, wc.hInstance, nullptr);
 
   // ウィンドウを表示する
   ShowWindow(hwnd, SW_SHOW);
@@ -71,9 +106,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
       DispatchMessage(&msg);
     } else {
       // ゲームの処理
-
     }
   }
+
+  Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
 
   return 0;
 }
